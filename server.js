6bc333604f888246e74bc8ee592ceb82a68e5719
carternadain <cnadain@gmail.com>
2024-10-09 19:30:22 -0700

    fix backend for server with cors

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const pizzaRoutes = require('./routes/pizzas'); // Pizza routes
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+  origin:'https://cryptic-thicket-49174-8acbdfd07325.herokuapp.com/'
+}));
+
 // Middleware
 app.use(cors());
 app.use(express.json());
